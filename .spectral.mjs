a78445df22e529e3850e1d111eaeee6a0b247724
jjlangefox <justin@apifox.com>
2026-03-25 12:11:55 +0800

    Update .spectral.mjs

--- .spectral.mjs
+++ .spectral.mjs
@@ -2,7 +2,7 @@ export default {
     rules: {                                                                                       
       'get-summary': {                                                                               
         message: 'GET operation must include a summary',                                             
-        severity: 'warn',                                                                            
+        severity: 'error',                                                                            
         given: '$.paths[*].get',                                                                     
         then: { field: 'summary', function: 'truthy' },                                              
       },                                                                                             
