603c85f0552594f7485131fa65aa50ec4c8c1914
jjlangefox <justin@apifox.com>
2026-03-25 09:47:09 +0800

    Update .spectral.mjs

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,33 +1,10 @@
-// .spectral.mjs
-export default {
-  extends: ['spectral:oas'],
-  rules: {
-    'operation-description': {
-      message: 'Operation must have a description',
-      severity: 'error',
-      given: '$.paths[*][*]',
-      then: {
-        field: 'description',
-        function: 'truthy',
-      },
-    },
-    'operation-tags': {
-      message: 'Operation must include tags',
-      severity: 'error',
-      given: '$.paths[*][*]',
-      then: {
-        field: 'tags',
-        function: 'truthy',
-      },
-    },
-    'get-summary': {
-      message: 'GET operation must include a summary',
-      severity: 'warn',
-      given: '$.paths[*].get',
-      then: {
-        field: 'summary',
-        function: 'truthy',
-      },
-    },
-  },
-};
+ export default {                                                                                   
+    rules: {                                                                                       
+      'get-summary': {                                                                               
+        message: 'GET operation must include a summary',                                             
+        severity: 'warn',                                                                            
+        given: '$.paths[*].get',                                                                     
+        then: { field: 'summary', function: 'truthy' },                                              
+      },                                                                                             
+    },                                                                                               
+  }; 
