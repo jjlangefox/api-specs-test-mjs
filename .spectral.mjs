d885690c537f7c7325dcace44728ff7ac0f251b6
jjlangefox <justin@apifox.com>
2026-03-25 09:42:23 +0800

    Update .spectral.mjs

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,38 +1,24 @@
-// .spectral.mjs
-import { ruleset } from '@stoplight/spectral-core';
-
-export default ruleset({
-  extends: ['spectral:oas'],
-  rules: {
-    'operation-description': {
-      description: 'All operations must have a description',
-      message: 'Operation must have a description',
-      severity: 'error',
-      given: '$.paths[*][*]',
-      then: {
-        field: 'description',
-        function: 'truthy',
-      },
-    },
-    'operation-tags': {
-      description: 'All operations must have at least one tag',
-      message: 'Operation must include tags',
-      severity: 'error',
-      given: '$.paths[*][*]',
-      then: {
-        field: 'tags',
-        function: 'truthy',
-      },
-    },
-    'get-summary': {
-      description: 'GET operations must have a summary',
-      message: 'GET operation must include a summary',
-      severity: 'warning',
-      given: '$.paths[*].get',
-      then: {
-        field: 'summary',
-        function: 'truthy',
-      },
-    },
-  },
-});
+// .spectral.mjs                                                                                   
+export default {                                                                                   
+    extends: [['spectral:oas', 'recommended']],                                                      
+    rules: {                                                                                         
+      'operation-description': {                                                                     
+        message: 'Operation must have a description',                                                
+        severity: 'error',                                                                           
+        given: '$.paths[*][*]',                                                                      
+        then: { field: 'description', function: 'truthy' },                                          
+      },                                                                                             
+      'operation-tags': {                                                                            
+        message: 'Operation must include tags',                                                      
+        severity: 'error',                                                                           
+        given: '$.paths[*][*]',                                                                      
+        then: { field: 'tags', function: 'truthy' },                                                 
+      },                                                                                             
+      'get-summary': {                                                                               
+        message: 'GET operation must include a summary',                                             
+        severity: 'warn',                                                                            
+        given: '$.paths[*].get',                                                                     
+        then: { field: 'summary', function: 'truthy' },                                              
+      },                                                                                             
+    },                                                                                               
+  };     
