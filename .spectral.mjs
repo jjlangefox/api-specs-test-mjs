aff2acf7f05ce6c64b2ca1be5cf42208fbac4da3
jjlangefox <justin@apifox.com>
2026-03-25 09:43:54 +0800

    Update .spectral.mjs

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,24 +1,33 @@
-// .spectral.mjs                                                                                   
-export default {                                                                                   
-    extends: [['spectral:oas', 'recommended']],                                                      
-    rules: {                                                                                         
-      'operation-description': {                                                                     
-        message: 'Operation must have a description',                                                
-        severity: 'error',                                                                           
-        given: '$.paths[*][*]',                                                                      
-        then: { field: 'description', function: 'truthy' },                                          
-      },                                                                                             
-      'operation-tags': {                                                                            
-        message: 'Operation must include tags',                                                      
-        severity: 'error',                                                                           
-        given: '$.paths[*][*]',                                                                      
-        then: { field: 'tags', function: 'truthy' },                                                 
-      },                                                                                             
-      'get-summary': {                                                                               
-        message: 'GET operation must include a summary',                                             
-        severity: 'warn',                                                                            
-        given: '$.paths[*].get',                                                                     
-        then: { field: 'summary', function: 'truthy' },                                              
-      },                                                                                             
-    },                                                                                               
-  };     
+// .spectral.mjs
+export default {
+  extends: ['spectral:oas'],
+  rules: {
+    'operation-description': {
+      message: 'Operation must have a description',
+      severity: 'error',
+      given: '$.paths[*][*]',
+      then: {
+        field: 'description',
+        function: 'truthy',
+      },
+    },
+    'operation-tags': {
+      message: 'Operation must include tags',
+      severity: 'error',
+      given: '$.paths[*][*]',
+      then: {
+        field: 'tags',
+        function: 'truthy',
+      },
+    },
+    'get-summary': {
+      message: 'GET operation must include a summary',
+      severity: 'warn',
+      given: '$.paths[*].get',
+      then: {
+        field: 'summary',
+        function: 'truthy',
+      },
+    },
+  },
+};
